f3bb9e7fbd0bd24655ec19dbe6840c703b33995b
Sh1nyuii <122020106+Sh1nyui123123@users.noreply.github.com>
2024-05-14 20:31:56 -0500

    Add files via upload

---
+++ Members/Becera/HotelBookingAPI/HotelBookingAPI/Controllers/BookingController.cs
@@ -0,0 +1,56 @@
+﻿using HotelBookingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private static readonly List<Booking> _bookings = new List<Booking>();
+
+        [HttpGet]
+        [Produces("application/json")]
+        public IEnumerable<Booking> Get()
+        {
+            return _bookings;
+        }
+
+        [HttpPost]
+        [Produces("application/json")]
+        public IActionResult Post(Booking booking)
+        {
+            _bookings.Add(booking);
+            return CreatedAtAction(nameof(GetById), new { id = booking.Id }, booking);
+        }
+
+        [HttpGet("{id}")]
+        [Produces("application/json")]
+        public IActionResult GetById(int id)
+        {
+            var booking = _bookings.Find(b => b.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(booking);
+        }
+
+        [HttpDelete("{id}")]
+        [Produces("application/json")]
+        public IActionResult Delete(int id)
+        {
+            var booking = _bookings.Find(b => b.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            _bookings.Remove(booking);
+            return NoContent();
+        }
+    }
+}
+
---
+++ Members/Becera/HotelBookingAPI/HotelBookingAPI/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBookingAPI.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string ?GuestName { get; set; }
+        public int RoomNumber { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set;}
+    }
+}
