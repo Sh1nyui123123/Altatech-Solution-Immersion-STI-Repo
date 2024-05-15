3851b6eb18cf3fbfd96291f76a43213aafca1e13
Sh1nyuii <122020106+Sh1nyui123123@users.noreply.github.com>
2024-05-14 20:38:33 -0500

    First Api Assignment

---
+++ Members/Becera/HelloWorld-API/HelloWorld/Controllers/HelloWorldler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldler : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> Post()
+        {
+            return "Hello World";
+        }
+    }
+}
